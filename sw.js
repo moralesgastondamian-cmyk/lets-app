c03a5ebe646979faead0aa6fd931254a4bfab6ba
gaston-lets <moralesgastondamian@gmail.com>
2026-04-05 20:03:26 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // ════════════════════════════════════════════════
 // INSTRUCCIÓN: Cada vez que subas un cambio al index.html,
 // incrementá el número de CACHE_VERSION para forzar la actualización.
-const CACHE_VERSION = 'lets-v6';
+const CACHE_VERSION = 'lets-v7';
 
 const ASSETS = [
   './',
