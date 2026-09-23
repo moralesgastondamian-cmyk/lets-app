493e772f1d47b80ac364d725079cad426b9289a0
gaston-lets <moralesgastondamian@gmail.com>
2026-09-23 20:45:55 -0300

    Add files via upload

--- V2/js/app.js
+++ V2/js/app.js
@@ -1,7 +1,7 @@
 // ════════════════════════════════════════════════
 //  app.js — punto de entrada principal
 // ════════════════════════════════════════════════
-export const VERSION = '2.17.0';
+export const VERSION = '2.18.0';
 export const BUILD = '2026-09-23';
 
 import { $ } from './core/dom.js';
